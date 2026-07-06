15f038bc6fa4592d25deed9de68d343bd4d80b90
Debbie English <49878317+debbieneaeraconsulting@users.noreply.github.com>
2026-07-06 15:53:13 -0600

    Adding Example with Signing

--- TrafficAuthSDKTest/TrafficAuthSDKTest/ContentView.swift
+++ TrafficAuthSDKTest/TrafficAuthSDKTest/ContentView.swift
@@ -13,62 +13,87 @@ struct ContentView: View {
     let token = ""
     let deviceId = ""
     let testMessageHex = "0381004003803200142F414DAB7F03E094E6E25EA5165C627D80007FFFFFFFFFFFF080FDFA1FA1007FFF8000000001040D0000002034001400600120000286274A12968000028632EB255340810101000301801631AFB5FC255D0F5080800257A35CBF8468396F188100000000A35CBF8468396F18815E6F5B00012A5022958400A9830101800348010300012000012600017F81821B84637FB2B24EB0F6AA3C284C91694C6BE4EAEED96E008C0C44213CD3B5A1A380805B11138F9D0178047569DA59EA3EEAF2CE0DD090D5B49C9A986E0F6499C5CA52A8E0E16E4CD05E3D345CE58F54A1E64E833470F3711613CFFD2C31067B71CC96"
+    let testUnsignedMessageHex = "00142F419191DA5A80CAE6E25EEA965C632900007FFFFFFFFFFFF080FDFA1FA1007FFF8000000001040D0000002034001400"
+    
+    let localSigning = LocalSigning.init(scmsEnv: ScmsEnvironment.PREPRODUCTION)
+    let tokenType = TokenType.DM_DASHBOARD;
     
     @State private var validationTime: Double?
+    @State private var signingTime: Double?
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
+            if let signingTime {
+                Text("Signing Time: \(signingTime) ms")
+            } else {
+                ProgressView()
+            }
 
-            if let validationTime {
+            if let validationTime{
                 Text("Validation Time: \(validationTime) ms")
             } else {
                 ProgressView()
             }
+            
+            
         }
         .padding()
         .task {
+            await setup()
             validationTime = await getValidationTime()
+            signingTime = await getSigningTime()
         }
     }
     
-    
-    public func getValidationTime() async -> Double{
-        let localSigning = LocalSigning.init(scmsEnv: ScmsEnvironment.PREPRODUCTION)
-        let tokenType = TokenType.DM_DASHBOARD;
-        
+    public func setup() async{
         do{
-            
             if(localSigning.getState() == SigningAPIState.NEED_CERTS){
                 try await localSigning.getDeviceCerts(token: token, type: tokenType, deviceId: deviceId)
             }
-            
         }
         catch{
             NSLog("Unable to Retrive Certificates \(error)")
         }
-        
-        let state = localSigning.getState()
-        NSLog("Signing API Status: \(state.rawValue)")
-        
-        
-        let data = convertHexToData(hexString: testMessageHex)
-        
+    }
+    
+    public func getSigningTime() async -> Double {
+        let data = convertHexToData(hexString: testUnsignedMessageHex)
         if(data != nil){
-            
+            do{
+                let start = CACurrentMediaTime()
+                let outputArray = try localSigning.sign(psid: 32, tbsOer: data!)
+                let end = CACurrentMediaTime()
+                let elapsedSeconds = end - start
+                NSLog("Execution time: \(elapsedSeconds) seconds")
+                
+                return roundSecondsToMillis(seconds: elapsedSeconds)
+            }catch{
+                NSLog("Signing Failed \(error)")
+            }
+        }
+        
+        
+        return -1.0
+    }
+    
+    
+    public func getValidationTime() async -> Double{
+        let data = convertHexToData(hexString: testMessageHex)
+        if(data != nil){
             do{
                 let start = CACurrentMediaTime()
                 let (valid, _) = try localSigning.validate(message:data!, shouldValidate:true)
-                NSLog("Validity: \(valid)")
+                
                 
                 let end = CACurrentMediaTime()
                 let elapsedSeconds = end - start
                 NSLog("Validate Execution time: \(elapsedSeconds) seconds")
+                NSLog("Validity: \(valid)")
                 
-                let milliseconds = round(elapsedSeconds * 1000000) / 1000
-                
-                return milliseconds
+                return roundSecondsToMillis(seconds:elapsedSeconds)
                 
             }catch{
                 NSLog("Error in Signature Verification \(error)")
@@ -76,6 +101,10 @@ struct ContentView: View {
         }
         return -1
         
+    }
+    
+    public func roundSecondsToMillis(seconds:Double)-> Double{
+        return round(seconds * 1000000) / 1000
     }
     
     public func convertHexToData(hexString:String) -> Data?{
