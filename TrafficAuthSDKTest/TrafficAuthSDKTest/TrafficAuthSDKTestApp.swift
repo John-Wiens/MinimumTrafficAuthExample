7851bf2ad52b106a8910a0defaf9c481d4d36e92
Debbie English <49878317+debbieneaeraconsulting@users.noreply.github.com>
2026-07-06 15:28:39 -0600

    Simple example of long decoding times

---
+++ TrafficAuthSDKTest/TrafficAuthSDKTest/TrafficAuthSDKTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  TrafficAuthSDKTestApp.swift
+//  TrafficAuthSDKTest
+//
+//  Created by michael on 7/6/26.
+//
+
+import SwiftUI
+
+
+@main
+struct TrafficAuthSDKTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrafficAuthSDKTest/TrafficAuthSDKTestTests/TrafficAuthSDKTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficAuthSDKTestTests.swift
+//  TrafficAuthSDKTestTests
+//
+//  Created by michael on 7/6/26.
+//
+
+import Testing
+@testable import TrafficAuthSDKTest
+
+struct TrafficAuthSDKTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
